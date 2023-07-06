3ea9916318cbd0b44da8b238841c2a5b2022ce49
wenleyan1 <wenleyang@yeah.net>
2023-07-06 12:51:41 -0400

    added RHIP angle caculations

--- caculateVEHSskeleton_bhillie.py
+++ caculateVEHSskeleton_bhillie.py
@@ -53,7 +53,7 @@ if __name__ == '__main__':
         raise NotImplementedError(f"More than one subject not implemented --> {subject_names}")
 
     ######################################## Read marker data ########################################
-    # upper body
+    ##### upper body #####
     C7_d = MarkerPoint(vicon.GetTrajectory(subject_names[0], 'C7_d'))
     SS = MarkerPoint(vicon.GetTrajectory(subject_names[0], 'SS'))
     RSHO_f = MarkerPoint(vicon.GetTrajectory(subject_names[0], 'RSHO_f'))
@@ -65,13 +65,31 @@ if __name__ == '__main__':
     # LME = MarkerPoint(vicon.GetTrajectory(subject_names[0], 'LME'))
     # LLE = MarkerPoint(vicon.GetTrajectory(subject_names[0], 'LLE'))
 
+    ##### upper body #####
+    RASIS = MarkerPoint(vicon.GetTrajectory(subject_names[0], 'RASIS'))
+    LASIS = MarkerPoint(vicon.GetTrajectory(subject_names[0], 'LASIS'))
+    RPSIS = MarkerPoint(vicon.GetTrajectory(subject_names[0], 'RPSIS'))
+    LPSIS = MarkerPoint(vicon.GetTrajectory(subject_names[0], 'LPSIS'))
+    LGT = MarkerPoint(vicon.GetTrajectory(subject_names[0], 'LGT'))
+    # RGT = MarkerPoint(vicon.GetTrajectory(subject_names[0], 'RGT'))
+    RLFC = MarkerPoint(vicon.GetTrajectory(subject_names[0], 'RLFC'))
+    RMFC = MarkerPoint(vicon.GetTrajectory(subject_names[0], 'RMFC'))
+    # LLFC = MarkerPoint(vicon.GetTrajectory(subject_names[0], 'LLFC'))
+    # LMFC = MarkerPoint(vicon.GetTrajectory(subject_names[0], 'LMFC'))
 
     ######################################## Create virtual markers ########################################
+    ##### upper body #####
     RSHOULDER = Point.mid_point(RSHO_f, RSHO_b)
     LSHOULDER = Point.mid_point(LSHO_f, LSHO_b)
     C7_m = Point.mid_point(C7_d, SS)
     # LELBOW = Point.mid_point(LME, LLE)
     RELBOW = Point.mid_point(RME, RLE)
+
+    ##### upper body #####
+    PELVIS_f = Point.mid_point(RASIS, LASIS)
+    PELVIS_b = Point.mid_point(RPSIS, LPSIS)
+    RHIP = Point.translate_point(LGT, Point.vector(RASIS, LASIS, normalize=2*25.4))  # offset 2 inches
+    RKNEE = Point.mid_point(RLFC, RMFC)
 
 
 
@@ -101,10 +119,28 @@ if __name__ == '__main__':
     # RHip angles
     try:
         zero_frame = 941
+        HIP_plane = Plane(RASIS, LASIS, PELVIS_b)
+        HIP_coord = CoordinateSystem3D()
+        HIP_coord.set_by_plane(HIP_plane, PELVIS_f, RASIS, sequence='zyx', axis_positive=True)
+        RHIP_angles = JointAngles()
+        RHIP_angles.set_zero_frame(zero_frame)
+        RKNEE_equivalentPt = Point.translate_point(PELVIS_f, Point.vector(RHIP, RKNEE), 100)
+        RHIP_angles.get_flex_abd(HIP_coord, RKNEE_equivalentPt, plane_seq=['xy', 'yz'])
 
+        ##### Visual for debugging #####
+        frame = 1000
+        print(f'RSHOULDER_angles:\n Flexion: {RSHOULDER_angles.flexion[frame]}, \n Abduction: {RSHOULDER_angles.abduction[frame]},\n Rotation: {RSHOULDER_angles.rotation[frame]}')
+        # Point.plot_points([RSHOULDER_coord.origin, RSHOULDER_coord.x_axis_end, RSHOULDER_coord.y_axis_end, RSHOULDER_coord.z_axis_end], frame=frame)
+        # RSHOULDER_angles.plot_angles(joint_name='Right Shoulder', frame_range=[1000, 1021])
+        render_dir = os.path.join(trial_name[0], 'render', trial_name[1], 'RSHOULDER')
+        RSHOULDER_angles.plot_angles_by_frame(render_dir, joint_name='Right Shoulder', frame_range=[941, 5756])
 
+    except:
+        print('RHip_angles failed')
 
     # Back angles
+
+
 
     # Head angles
 
