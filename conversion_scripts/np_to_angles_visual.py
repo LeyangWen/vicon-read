correct angle sign

conversion_scripts/np_to_angles_visual.py
@@ -7,14 +7,16 @@ from MB_np_to_visual import MB_input_pose_file_loader
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    # parser.add_argument('--config_file', type=str, default=r'config/experiment_config/VEHS-6D-MB.yaml')
-    # parser.add_argument('--skeleton_file', type=str, default=r'config/VEHS_ErgoSkeleton_info/Ergo-Skeleton-66.yaml')
+    # parser.add_argument('--config_file', type=str, default=r'config/experiment_config/37kpts/Inference-RTMPose-MB-20fps-VEHS7M.yaml')
+    # parser.add_argument('--overlay_GT', type=bool, default=True)
+    # parser.add_argument('--debug_mode', default=True)
 
-    parser.add_argument('--config_file', type=str, default=r'config/experiment_config/37kpts/Inference-RTMPose-MB-20fps-VEHS7M.yaml')
-    parser.add_argument('--skeleton_file', type=str, default=r'config/VEHS_ErgoSkeleton_info/Ergo-Skeleton-37.yaml')
-
-    parser.add_argument('--MB_data_stride', type=int, default=243)
+    parser.add_argument('--config_file', type=str, default=r'config/experiment_config/37kpts/Inference-RTMPose-MB-20fps-Industry.yaml')
+    parser.add_argument('--overlay_GT', type=bool, default=False)
     parser.add_argument('--debug_mode', default=False)
+
+    parser.add_argument('--skeleton_file', type=str, default=r'config/VEHS_ErgoSkeleton_info/Ergo-Skeleton-37.yaml')
+    parser.add_argument('--MB_data_stride', type=int, default=243)
     parser.add_argument('--merge_lr', default=True)
 
     # parser.add_argument('--name_list', type=list, default=[])
@@ -25,7 +27,7 @@ def parse_args():
         args.GT_file = data['GT_file']
         args.eval_key = data['eval_key']
         args.estimate_file = data['estimate_file']
-        args.output_dir = os.path.join(os.path.dirname(args.estimate_file), 'results')
+        args.output_dir = os.path.join(os.path.dirname(args.estimate_file), 'angles')
         if not os.path.exists(args.output_dir):
             os.makedirs(args.output_dir)
     return args
@@ -78,23 +80,24 @@ if __name__ == '__main__':
         print(estimate_pose.shape, GT_pose.shape)
         assert estimate_pose.shape == GT_pose.shape, f"GT_pose.shape: {GT_pose.shape}, estimate_pose.shape: {estimate_pose.shape}, they should be the same"
     else:
-        GT_pose = MB_input_pose_file_loader(args)
         estimate_pose = MB_output_pose_file_loader(args)
-        assert GT_pose.shape == estimate_pose.shape, f"GT_pose.shape: {GT_pose.shape}, estimate_pose.shape: {estimate_pose.shape}, they should be the same"
-
+        GT_pose = MB_input_pose_file_loader(args)
+        if args.overlay_GT:
+            assert GT_pose.shape == estimate_pose.shape, f"GT_pose.shape: {GT_pose.shape}, estimate_pose.shape: {estimate_pose.shape}, they should be the same"
 
     if args.debug_mode:
-        small_sample = 7500
-        GT_pose = GT_pose[:small_sample]
+        small_sample = 243*6
+        GT_pose = GT_pose[:small_sample] if GT_pose is not None else None
         estimate_pose = estimate_pose[:small_sample]
 
-    # Step 1: calculate GT angles
-    GT_skeleton = VEHSErgoSkeleton_angles(args.skeleton_file)
-    GT_skeleton.load_name_list_and_np_points(args.name_list, GT_pose)
-    GT_ergo_angles = {}
-    for angle_name in GT_skeleton.angle_names:  # calling the angle calculation methods in skeleton class
-        class_method_name = f'{angle_name}_angles'
-        GT_ergo_angles[angle_name] = getattr(GT_skeleton, class_method_name)()
+    if args.overlay_GT:
+        # Step 1: calculate GT angles
+        GT_skeleton = VEHSErgoSkeleton_angles(args.skeleton_file)
+        GT_skeleton.load_name_list_and_np_points(args.name_list, GT_pose)
+        GT_ergo_angles = {}
+        for angle_name in GT_skeleton.angle_names:  # calling the angle calculation methods in skeleton class
+            class_method_name = f'{angle_name}_angles'
+            GT_ergo_angles[angle_name] = getattr(GT_skeleton, class_method_name)()
 
     # Step 2: calculate MB angles
     estimate_skeleton = VEHSErgoSkeleton_angles(args.skeleton_file)
@@ -105,158 +108,34 @@ if __name__ == '__main__':
         estimate_ergo_angles[angle_name] = getattr(estimate_skeleton, class_method_name)()
 
     # Step 3: visualize
-    
-    # # Hi Veeru, I used this to visualize the 3D pose frame by frame
     # frame = 10210
     # frame = 10180
     # GT_skeleton.plot_3d_pose_frame(frame)
     # estimate_skeleton.plot_3d_pose_frame(frame)
 
-    frame_range = [101800, 102200]
-    log = []
-    anova_results = []
-    target_angles = GT_skeleton.angle_names
+    frame_range = None #[0, 60*3*20]
+    target_angles = estimate_skeleton.angle_names
     # target_angles = ['right_shoulder']
+    print(f"target angles: {target_angles}")
+    # Single thread
     for angle_index, this_angle_name in enumerate(target_angles):
-        # plot angles
+        # plot angles in single image
         # GT_fig, GT_ax = GT_ergo_angles[this_angle_name].plot_angles(joint_name=f"GT-{this_angle_name}", frame_range=frame_range, alpha=0.75, colors=['g', 'g', 'g'])
         # estimate_fig, _ = estimate_ergo_angles[this_angle_name].plot_angles(joint_name=f"Est-{this_angle_name}", frame_range=frame_range, alpha=0.75, colors=['r', 'r', 'r'], overlay=[GT_fig, GT_ax])
         # plt.show()
-        # GT_fig.savefig(f'frames/MB_angles/GT-{this_angle_name}.png')
+        # # GT_fig.savefig(f'frames/MB_angles/GT-{this_angle_name}.png')
         # estimate_fig.savefig(f'frames/MB_angles/Est-{this_angle_name}.png')
 
-        ergo_angle_name = ['flexion', 'abduction', 'rotation']
+        # plot angles as video frames
         print_ergo_names = getattr(estimate_ergo_angles[this_angle_name], 'ergo_name')
-        print_angle_name = this_angle_name.replace('_', '').replace('right', 'R-').replace('left', 'L-').capitalize()
-        flexion_errors = np.array([])
-        abduction_errors = np.array([])
-        rotation_errors = np.array([])
-        for this_ergo_angle in ergo_angle_name:
-            ja1 = getattr(estimate_ergo_angles[this_angle_name], this_ergo_angle)
-            ja2 = getattr(GT_ergo_angles[this_angle_name], this_ergo_angle)
-
-            if args.merge_lr:
-                if 'right' in this_angle_name:
-                    ja1_l = getattr(estimate_ergo_angles[this_angle_name.replace('right', 'left')], this_ergo_angle)
-                    ja2_l = getattr(GT_ergo_angles[this_angle_name.replace('right', 'left')], this_ergo_angle)
-                    try:
-                        ja1 = np.concatenate([ja1, ja1_l])
-                        ja2 = np.concatenate([ja2, ja2_l])
-                    except(ValueError):
-                        continue
-                    print_angle_name = print_angle_name.replace('R-', '').capitalize()
-                elif 'left' in this_angle_name:
-                    continue
-            print_ergo_name = print_ergo_names[this_ergo_angle].capitalize()
-            if ja1 is not None:
-                print("=====================================")
-                print(f'{this_angle_name} - {this_ergo_angle}')
-                # bland-Altman plot
-                if not os.path.exists(os.path.join(args.output_dir, 'BA_plots', args.eval_key)):
-                    os.makedirs(os.path.join(args.output_dir, 'BA_plots', args.eval_key))
-                md, sd = bland_altman_plot(ja1, ja2, title=f'{print_angle_name}: {print_ergo_name}',
-                                           save_path=os.path.join(args.output_dir,f'BA_plots/{args.eval_key}/{angle_index}-{this_angle_name}-{this_ergo_angle}.png'))
-                                           #save_path=f'frames/MB_angles/BA_plots/{angle_index}-{this_angle_name}-{this_ergo_angle}.png')
-                print(f'Bland Altman: md: {md:.2f}, sd: {sd:.2f}')
-
-                RMSE = root_mean_squared_error(ja1, ja2)
-                MAE = mean_absolute_error(ja1, ja2)
-                median_AE = median_absolute_error(ja1, ja2)
-
-                angle_compare = AngleCompare(ja1, ja2)
-                # 1,435,236 frames in test set, use Rice rule --> approx 225 bins
-                # make folder if not exits
-                if not os.path.exists(os.path.join(args.output_dir, 'histograms', args.eval_key)):
-                    os.makedirs(os.path.join(args.output_dir, 'histograms', args.eval_key))
-                plot_error_histogram(angle_compare.diff_deg, bins=225, title=f'{print_angle_name}: {print_ergo_name}',
-                                     save_path=os.path.join(args.output_dir, f'histograms/{args.eval_key}/{angle_index}-{this_angle_name}-{this_ergo_angle}_hist.png'),
-                                     plot_normal_curve=angle_compare.plot_normal_curve)
-                                     # save_path=f'frames/MB_angles/histograms/{angle_index}-{this_angle_name}-{this_ergo_angle}_hist.png',
-                error_dist = analyze_error_distribution(angle_compare.diff_deg)
-                print(f'Error distribution: {error_dist}')
-
-                # error_dist = analyze_error_distribution(angle_compare.diff_inliers_deg)
-                # print(f'Error distribution: {error_dist}')
-
-                np.nanstd(ja1, axis=0)
-                np.nanstd(ja1)
-
-                # Calculate errors
-                errors = ja1 - ja2
-                if this_ergo_angle == 'flexion':
-                    flexion_errors = errors.copy()
-                elif this_ergo_angle == 'abduction':
-                    abduction_errors = errors.copy()
-                elif this_ergo_angle == 'rotation':
-                    rotation_errors = errors.copy()
-
-
-                print(f'MAE: {MAE:.2f}, RMSE: {RMSE:.2f}, median: {angle_compare.median_deg:.2f}')
-                # this_log = [this_angle_name, this_ergo_angle, md, sd, MAE, RMSE]
-                this_log = [print_angle_name, print_ergo_name, MAE, median_AE, RMSE, md, sd, error_dist['skewness'], error_dist['kurtosis'], angle_compare.median_deg, error_dist['IQR']]
-                log.append(this_log)
-
-        # # Perform ANOVA for each joint
-        # if flexion_errors.size > 0 and abduction_errors.size > 0 and rotation_errors.size > 0:
-        #     cut = 1500
-        #     f_stat, p_value = f_oneway(flexion_errors[:cut], abduction_errors[:cut] , rotation_errors[:cut])
-        #     anova_results.append((this_angle_name, f_stat, p_value))
-        # else:
-        #     f_stat, p_value = np.nan, np.nan
-
-
-
-    # print log as csv in console
-    header = ["angle_name", "ergo_angle", "MAE", "median_AE", "RMSE", "diff_md", "dif_sd", "skewness", "kurtosis", "median", "IQR"]
-    for i in log:
-        for j in i:
-            print(j, end=",")
-        print()
-    # also save log as csv
-    with open(os.path.join(args.output_dir, f'{args.eval_key}_log.csv'), 'w') as f:
-        f.write(f"{header}\n")
-        for i in log:
-            for j in i:
-                f.write(str(j)+",")
-            f.write("\n")
-
-    print("ANOVA Results:")
-    print("Angle Name, F-Statistic, P-Value")
-    for result in anova_results:
-        print(f"{result[0]}, {result[1]:}, {result[2]}")
-
-    print(f"Store location: {args.output_dir}")
-# generate merged bland-altman plot for left and right
-
-
-""" powershell batch crop png files
-cd frames/MB_angles/BA_plots
-Remove-Item .\cropped\* -Recurse
-$files = Get-ChildItem
-foreach ($file in $files) {
-    ffmpeg -i $file -vf "crop=515:450:74:22" "cropped/$file"
-}
-"""
-""" in mac
-#!/bin/bash
-
-# Create the cropped directory if it doesn't exist
-mkdir -p cropped
-
-# Remove existing files in the cropped directory
-rm -rf cropped/*
-
-# Loop through all PNG files in the current directory
-for file in *.png; do
-    # Crop using FFmpeg
-    ffmpeg -i "$file" -vf "crop=515:450:74:22" "cropped/$file"
-done
-
-"""
-
-""" ffmpeg crop command
-ffmpeg -i back-abduction.png -vf "crop=515:430:74:22" output.jpg
-"""
-
-
+        render_dir = os.path.join(args.output_dir, f'{this_angle_name}')
+        if args.overlay_GT:
+            frame_range_max = 1200  # 1 min, around 5 243 clips
+            estimate_ergo_angles[this_angle_name].plot_angles_by_frame(render_dir, joint_name=f"{this_angle_name}", alpha=0.75, colors=['r', 'r', 'r'], frame_range=frame_range, frame_range_max=frame_range_max, label="Inference",
+                                                                       angle_names=list(print_ergo_names.values()), overlay=GT_ergo_angles[this_angle_name], overlay_colors=['g', 'g', 'g'], fps=20, x_tick_s=15)
+        else:
+            # frame_range_max = 243
+            frame_range_max = list(np.array([2,2,1,2,1,2,2,2,1,2,2,2])*243)
+            estimate_ergo_angles[this_angle_name].plot_angles_by_frame(render_dir, joint_name=f"{this_angle_name}", alpha=0.75, colors=['r', 'r', 'r'], frame_range=frame_range, frame_range_max=frame_range_max,
+                                                                       angle_names=list(print_ergo_names.values()), fps=20, x_tick_s=2)
 